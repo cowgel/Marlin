d78375de6f4214db5b5fc3f9af810c2c8fcbd7e0
Samuel Gross <sam@cowgel.com>
2022-06-07 21:56:15 -0400

    updated LEVEL_BED_CORNERS to LCD_BED_TRAMMING

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2496,8 +2496,7 @@
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
-#define LEVEL_BED_CORNERS
-//#define LCD_BED_TRAMMING
+#define LCD_BED_TRAMMING
 
 #if ENABLED(LCD_BED_TRAMMING)
   #define BED_TRAMMING_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
